08ea77bfc77e222bf5c458df49a6bcc5cc2afa15
Michael Ganger <mganger747@gmail.com>
2020-04-04 16:18:21 -0400

    Did more stuff

--- pyjama/server.py
+++ pyjama/server.py
@@ -1,30 +1,34 @@
 from .udp import udp_writer, udp_reader
-from time import time
+from time import time, sleep
 
 class Server:
     def __init__(self):
         self.clients = {}
-        self.last_update = None
+        self.next_update = None
 
-    def check_and_notify(self, addr):
+    def add(self, addr):
         ip, port = addr
         if ip not in self.clients:
             self.clients[ip] = port
-            self.notify()
-        if self.should_update():
-            self.notify()
+            self.next_update = time()
 
     def notify(self):
         print("notifying!")
-        self.last_update = time()
+        self.next_update = time() + 0.1
 
     def should_update(self):
         return len(self.clients) > 0 \
-                and self.last_update is not None \
-                and time() - self.last_update > 0.1
+                and self.next_update is not None \
+                and time() >= self.next_update
 
 if __name__ == "__main__":
     server = Server()
-    for data, addr in udp_reader():
-        print(data, addr)
-        server.check_and_notify(addr)
+    with udp_reader() as messages:
+        while True:
+            if not messages.empty():
+                data, addr = messages.get()
+                print(data, addr)
+                server.add(addr)
+            if server.should_update():
+                server.notify()
+            sleep(0.01)
--- pyjama/udp.py
+++ pyjama/udp.py
@@ -1,12 +1,21 @@
 import socket
 from time import sleep
+from queue import Queue
+from threading import Thread
+from contextlib import contextmanager
 
+@contextmanager
 def udp_reader(ip=None, port=4464, buf=1024):
     ip = ip or '0.0.0.0'
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((ip, port))
-    while True:
-        yield sock.recvfrom(buf)
+    q = Queue()
+    def _inner():
+        while True:
+            q.put(sock.recvfrom(buf))
+    thread = Thread(target=_inner)
+    thread.start()
+    yield q
 
 def udp_writer(data, ip, port=4464):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
